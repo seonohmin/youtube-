fc6debc7bf70b3a2f8eda4c268daac002753a656
seonohmin <seon.o.min@gmail.com>
2023-09-14 14:15:42 +0900

    youtube clone coding

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,20 +1,59 @@
 import axios from "axios";
 
-export default class FakeYoutube {
-  constructor() {}
+export default class Youtube {
+  constructor(apiClient) {
+    this.apiClient = apiClient;
+  }
 
   async search(keyword) {
     return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
   }
 
-  async #searchByKeyword() {
-    return axios
-      .get(`/videos/search.json`)
-      .then((res) => res.data.items)
-      .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })));
+  async channelImageURL(id) {
+    return this.apiClient
+      .channels({ params: { part: "snippet", id } })
+      .then((res) => res.data.items[0].snippet.thumbnails.default.url);
+  }
+
+  async relatedVideos(id) {
+    return this.apiClient
+      .search({
+        params: {
+          part: "snippet",
+          maxResults: 25,
+          type: "video",
+          videoId: id,
+        },
+      })
+      .then((res) =>
+        res.data.items.map((item) => ({ ...item, id: item.id.videoId }))
+      );
+  }
+
+  async #searchByKeyword(keyword) {
+    return this.apiClient
+      .search({
+        params: {
+          part: "snippet",
+          maxResults: 25,
+          type: "video",
+          q: keyword,
+        },
+      })
+      .then((res) =>
+        res.data.items.map((item) => ({ ...item, id: item.id.videoId }))
+      );
   }
 
   async #mostPopular() {
-    return axios.get(`/videos/popular.json`).then((res) => res.data.items);
+    return this.apiClient
+      .videos({
+        params: {
+          part: "snippet",
+          maxResults: 25,
+          chart: "mostPopular",
+        },
+      })
+      .then((res) => res.data.items);
   }
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,25 +2,25 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
-import FakeYoutube from '../api/fakeYoutube';
+// import Youtube from '../api/youtube';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+// import FakeYoutube from '../api/fakeYoutubeClient';
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const { 
     isLoading, 
     error, 
     data: videos 
-  } = useQuery(['videos', keyword], () => {
-    const youtube = new FakeYoutube();
-    return youtube.search(keyword);
-  });
+  } = useQuery(['videos', keyword], () => youtube.search(keyword), {staleTime: 1000 * 60 * 1});
+ 
   return (
     <>
-      <div>Videos { keyword ? `${keyword}` : `Hot Trend`}</div>
       {isLoading && <p>loading</p>}
       {error && <p>에러발생..</p>}
       {videos && (
-        <ul>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-10'>
           {videos.map((video) => (
             <VideoCard key={video.id} video={video}/>
           ))}
